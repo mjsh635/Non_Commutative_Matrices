80973cd19e3483d90ab353d8f075992ef12b4f60
MSH <mjsh635@gmail.com>
2020-02-11 13:05:46 -0500

    setup structure of matrix class

---
+++ Matrices.py
@@ -0,0 +1,17 @@
+class Matrices:
+    """create a matrix of size row/columns
+        create and return all the random values for the matrix
+        populate that matrix with random values
+        return the matrix
+    """
+    def _create_matrix(self, rows:int, columns:int):
+        pass
+
+    def _random_matrix_values(self, num_of_rows:int, num_of_columns:int):
+        pass
+
+    def _populate_matrix(self, tbd):
+        pass
+
+    def get_matrix(self):
+        pass
