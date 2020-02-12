bd759aea936c0364bd5ecb2599fc2c1b74cd368b
MSH <mjsh635@gmail.com>
2020-02-12 12:48:59 -0500

    new Test values

--- Matrices.py
+++ Matrices.py
@@ -91,8 +91,8 @@ class Matrix:
             return "The number of columns in the first matrix should match the rows in the second"
 
 
-m1 = Matrix(2,3)
-m2 = Matrix(3,2,2)
+m1 = Matrix(2,1)
+m2 = Matrix(1,4,2)
 print(m1.get_matrix())
 print(m2.get_matrix())
 print(m1*m2)
